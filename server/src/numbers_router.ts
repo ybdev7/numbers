2f80d75b9f3d88731ec21f62994273d1d68cde12
ybdev7 <99820567+ybdev7@users.noreply.github.com>
2022-03-02 11:12:30 -0500

    added prettier

--- server/src/numbers_router.ts
+++ server/src/numbers_router.ts
@@ -9,56 +9,57 @@ export const numbersRouter = express.Router();
 /**
  * Returns a list of facts relevant to the provided number
  */
-numbersRouter.get("/funfacts/:num", async (inRequest, inResponse) => {
-    console.log(`GET /numbers/funfacts/:num with num=${inRequest.params.num}`);
+numbersRouter.get('/funfacts/:num', async (inRequest, inResponse) => {
+  console.log(`GET /numbers/funfacts/:num with num=${inRequest.params.num}`);
   try {
     const numbersWorker: NumbersWorker = new NumbersWorker();
-    const numberInfo: INumberInfo = await numbersWorker.listFunFacts(inRequest.params.num);
-    console.log(`GET /numbers/funfacts/:num - OK. Number choosen =  ${numberInfo._number}`);
+    const numberInfo: INumberInfo = await numbersWorker.listFunFacts(
+      inRequest.params.num
+    );
+    console.log(
+      `GET /numbers/funfacts/:num - OK. Number choosen =  ${numberInfo._number}`
+    );
     inResponse.json(numberInfo);
   } catch (inError) {
-    console.log("GET /numbers: Error", inError);
-    inResponse.send("error");
+    console.log('GET /numbers: Error', inError);
+    inResponse.send('error');
   }
-}
-);
-
+});
 
 /**
  * Randomly chooses a number between 0 and 10000 and returns a INumberInfo for it
  */
-numbersRouter.get("/", async (inRequest, inResponse) => {
-    console.log("GET /numbers");
+numbersRouter.get('/', async (inRequest, inResponse) => {
+  console.log('GET /numbers');
   try {
     const numbersWorker: NumbersWorker = new NumbersWorker();
-    const numberInfo: INumberInfo = await numbersWorker.listInfo(Math.round(Math.random()*10000));
+    const numberInfo: INumberInfo = await numbersWorker.listInfo(
+      Math.round(Math.random() * 10000)
+    );
     console.log(`GET /numbers: OK. Number choosen =  ${numberInfo._number}`);
     inResponse.json(numberInfo);
   } catch (inError) {
-    console.log("GET /numbers: Error", inError);
-    inResponse.send("error");
+    console.log('GET /numbers: Error', inError);
+    inResponse.send('error');
   }
-}
-);
+});
 
 /**
  * Returns INumberInfo for a specified number. Throws an error if the parameter provided is nor a proper integer
  */
-numbersRouter.get("/:num", async (inRequest, inResponse) => {
-    console.log(`GET /numbers/:num with num=${inRequest.params.num}`);
+numbersRouter.get('/:num', async (inRequest, inResponse) => {
+  console.log(`GET /numbers/:num with num=${inRequest.params.num}`);
   try {
     const numbersWorker: NumbersWorker = new NumbersWorker();
-    const numberInfo: INumberInfo = await numbersWorker.listInfo(inRequest.params.num);
+    const numberInfo: INumberInfo = await numbersWorker.listInfo(
+      inRequest.params.num
+    );
     console.log(`GET /numbers: OK. Number choosen =  ${numberInfo._number}`);
     inResponse.json(numberInfo);
   } catch (inError) {
-    console.log("GET /numbers: Error", inError);
-    inResponse.send("error");
+    console.log('GET /numbers: Error', inError);
+    inResponse.send('error');
   }
-}
-);
-
-
+});
 
 module.exports = numbersRouter;
-
--- server/src/numbers_worker.ts
+++ server/src/numbers_worker.ts
@@ -1,114 +1,100 @@
-import { IFunFact, PrimeFact, PSFact } from "./facts";
-
+import { IFunFact, PrimeFact, PSFact } from './facts';
 
 export interface INumberInfo {
-    _number : number;
-    isEven?: boolean;
-    isPrime? : boolean;
-    divisors? : Array<number>;
-    funFacts? : Array<string>;
+  _number: number;
+  isEven?: boolean;
+  isPrime?: boolean;
+  divisors?: Array<number>;
+  funFacts?: Array<string>;
 }
 
 export interface IFactsInfo {
-    _number : number;
-    facts? : Array<IFunFact>;
+  _number: number;
+  facts?: Array<IFunFact>;
 }
 
 /**
  * Provides the logic for populating INumberInfo
  */
 export class NumbersWorker {
-    
-    constructor()
-    {
-        
+  constructor() {}
+
+  private static getDivisors(num: number): Array<number> {
+    let divisors = new Array<number>();
+
+    for (let i: number = 1; i <= num; i++) {
+      if (num % i == 0 && !divisors.find((item) => item == i)) divisors.push(i);
     }
 
+    console.log(divisors);
+    return divisors;
+  }
 
-    private static getDivisors(num : number) : Array<number>
-    {
-        let divisors = new Array<number>();
+  /**
+   * Lists integer info for integers in [0-99999] range
+   * @param num a valid number [0-99999] or a string representation of one
+   * @returns basic integer information
+   */
+  public listInfo(num: number | string): Promise<INumberInfo> {
+    console.log(`in NumbersWorker.listInfo(${num})`);
 
-        for(let i : number =1; i<=num; i++)
-        {
-            if (num%i == 0 && !divisors.find(item=> item==i))
-                divisors.push(i);
-        }
+    return new Promise<INumberInfo>((inResolve, inReject) => {
+      try {
+        //numbers only, no more then 5 charachres long
+        const regex = new RegExp('^[0-9]{1,5}$');
+        if (typeof num === 'string' && !regex.test(num))
+          throw new Error('Please provide a number');
 
-        console.log(divisors);
-        return divisors;
+        const _num = typeof num === 'string' ? parseInt(num) : num;
+        if (isNaN(_num)) throw new Error('Please provide a number');
+
+        const divisors = NumbersWorker.getDivisors(_num);
+        const info: INumberInfo = {
+          _number: _num,
+          isEven: _num % 2 == 0,
+          isPrime: divisors.length <= 2,
+          divisors: divisors
+        };
+        inResolve(info);
+      } catch (error: any) {
+        console.log('ERROR in NumbersWorker.listInfo(): ', error);
+        inReject(error);
+      }
+    });
+  }
+
+  public listFunFacts(num: number | string): Promise<IFactsInfo> {
+    console.log(`in NumbersWorker.listFunFacts(${num})`);
+
+    return new Promise<IFactsInfo>((inResolve, inReject) => {
+      try {
+        //numbers only, no more then 5 charachres long
+        const regex = new RegExp('^[0-9]{1,5}$');
+        if (typeof num === 'string' && !regex.test(num))
+          throw new Error('Please provide a number');
+
+        const _num = typeof num === 'string' ? parseInt(num) : num;
+        if (isNaN(_num)) throw new Error('Please provide a number');
+
+        const info: IFactsInfo = {
+          _number: _num,
+          facts: NumbersWorker.getFacts(_num)
+        };
+        inResolve(info);
+      } catch (error: any) {
+        console.log('ERROR in NumbersWorker.listInfo(): ', error);
+        inReject(error);
+      }
+    });
+  }
+
+  private static getFacts(_num: number): IFunFact[] | undefined {
+    try {
+      return [new PrimeFact(_num), new PSFact(_num)];
+    } catch (e) {
+      throw new Error('Error getting facts');
     }
-
-    /**
-     * Lists integer info for integers in [0-99999] range
-     * @param num a valid number [0-99999] or a string representation of one
-     * @returns basic integer information
-     */
-    public listInfo(num : number | string): Promise<INumberInfo> {
-
-        console.log(`in NumbersWorker.listInfo(${num})`);
-    
-        
-        return new Promise<INumberInfo>((inResolve, inReject) => {
-            try {
-                //numbers only, no more then 5 charachres long
-                const regex = new RegExp('^[0-9]{1,5}$');
-                if (( typeof num === "string" ) && (!regex.test(num)))
-                    throw new Error('Please provide a number');
-
-                const _num = typeof num === "string" ? parseInt(num) : num;
-                if (isNaN(_num))
-                    throw new Error('Please provide a number');
-                
-                const divisors = NumbersWorker.getDivisors(_num);
-                const info : INumberInfo = { _number: _num, isEven: _num%2==0, isPrime: divisors.length<=2, divisors:divisors};
-                inResolve(info);
-           }
-            catch(error: any)
-            {
-                console.log("ERROR in NumbersWorker.listInfo(): ", error);
-                inReject(error);
-            }
-      });
-    }
-     
-    public listFunFacts(num : number | string) : Promise<IFactsInfo>
-    {
-        console.log(`in NumbersWorker.listFunFacts(${num})`);
-    
-        
-        return new Promise<IFactsInfo>((inResolve, inReject) => {
-            try {
-                //numbers only, no more then 5 charachres long
-                const regex = new RegExp('^[0-9]{1,5}$');
-                if (( typeof num === "string" ) && (!regex.test(num)))
-                    throw new Error('Please provide a number');
-
-                const _num = typeof num === "string" ? parseInt(num) : num;
-                if (isNaN(_num))
-                    throw new Error('Please provide a number');
-                
-                const info : IFactsInfo = { _number: _num, facts:NumbersWorker.getFacts(_num)};
-                inResolve(info);
-           }
-            catch(error: any)
-            {
-                console.log("ERROR in NumbersWorker.listInfo(): ", error);
-                inReject(error);
-            }
-      });
-    }
-
-    private static getFacts(_num: number): IFunFact[] | undefined {
-
-        try {
-        return [new PrimeFact(_num), new PSFact(_num)];
-        }
-        catch(e)
-        {
-            throw( new Error("Error getting facts"));
-        }
-    }
+  }
 }
 
 // function getFacts(_num: number): IFunFact[] | undefined {
